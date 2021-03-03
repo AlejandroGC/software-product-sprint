6b29c27fe0131a3b27c02bde02ce367750fca91b
AlexGzz <adgonzalezcarrillo+r@gmail.com>
2021-03-03 04:12:22 +0000

    Change in WebServlet url and modify what the servlet sends

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/gallery.html")
+@WebServlet("/helloWorld")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h2>Hello world! My name is Alejandro and this is a servlet.</h2>");
+    response.getWriter().println("Hello world! My name is Alejandro and this is a servlet.");
   }
 }
