983947c983358df05bd8f452c780faf792b7fb97
AlexGzz <adgonzalezcarrillo+r@gmail.com>
2021-03-04 10:41:14 +0000

    Create function to manipulate the json information an display it

---
+++ portfolio/src/main/webapp/json.js
@@ -0,0 +1,14 @@
+// Function that receives the facts as a JSON from the servlet and display them at the webpage.
+async function getFacts() {
+    const getResponse = await fetch('/facts');
+    const facts = await getResponse.json();
+
+    const factsContainer = document.getElementById('facts-container');
+    factsContainer.innerHTML = '';
+
+    factsContainer.appendChild(createListElement('Fact #1: ' + facts[0]));
+    factsContainer.appendChild(createListElement('Fact #2: ' + facts[1]));
+    factsContainer.appendChild(createListElement('Fact #3: ' + facts[2]));
+
+}
+
