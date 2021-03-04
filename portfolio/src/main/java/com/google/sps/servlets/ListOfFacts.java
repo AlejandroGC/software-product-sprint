9ac95d19a403be18f4677850cd1f781b13566169
AlexGzz <adgonzalezcarrillo+r@gmail.com>
2021-03-04 10:36:55 +0000

    Create an array with facts and return this as a json using GSON

---
+++ portfolio/src/main/java/com/google/sps/servlets/ListOfFacts.java
@@ -0,0 +1,32 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
+@WebServlet("/facts")
+public class ListOfFacts extends HttpServlet { 
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        ArrayList<String> array = new ArrayList<String>(Arrays.asList("prueba", "prueba2", "prueba3"));
+        String json = convertToJsonUsingGson(array);
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+    }
+
+    public String convertToJsonUsingGson(ArrayList array) {
+        Gson gson = new Gson();
+        String json = gson.toJson(array);
+        return json;
+    }
+}
