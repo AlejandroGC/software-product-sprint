ba216fb77545505345df31308d9f9ca0ed73e5e8
AlexGzz <adgonzalezcarrillo+r@gmail.com>
2021-03-05 08:45:56 +0000

    Delete the function createlist. getFacts now returns a random fact

--- portfolio/src/main/webapp/json.js
+++ portfolio/src/main/webapp/json.js
@@ -3,18 +3,10 @@ async function getFacts() {
     const getResponse = await fetch('/facts');
     const facts = await getResponse.json();
 
+    const index = Math.floor(Math.random() * 3);
     const factsContainer = document.getElementById('facts-container');
-    factsContainer.innerHTML = '';
+    factsContainer.innerText = '';
 
-    factsContainer.appendChild(createListElement('Fact #1: ' + facts[0]));
-    factsContainer.appendChild(createListElement('Fact #2: ' + facts[1]));
-    factsContainer.appendChild(createListElement('Fact #3: ' + facts[2]));
+    factsContainer.innerText = facts[index];
 
-}
-
-// Function that creates a list from a received text.
-function createListElement(text) {
-    const liElement = document.createElement('li');
-    liElement.innerText = text;
-    return liElement;
 }
